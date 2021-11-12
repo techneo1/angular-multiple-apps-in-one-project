ea13ec9bc4f632961390139c338fbb68df2e968b
Srikanth Shet <techneo1@gmail.com>
2021-11-12 14:55:03 +0530

    Change names of both AppComponents

--- projects/chat/src/app/app.module.ts
+++ projects/chat/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ChatAppComponent } from './app.component';
 import { View1Component } from './view1/view1.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    ChatAppComponent,
     View1Component,
   ],
   imports: [
@@ -15,7 +15,7 @@ import { View1Component } from './view1/view1.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ChatAppComponent]
 })
 export class AppModule { }
 
