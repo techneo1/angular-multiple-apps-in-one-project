5273f93ec42cdde79655ec20df67124ac1cc1f45
Srikanth Shet <techneo1@gmail.com>
2021-11-12 14:58:06 +0530

    Replace modules with component in the main app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardSharedModule } from 'projects/dashboard/src/app/app.module';
 import { ChatSharedModule } from 'projects/chat/src/app/app.module';
+import { DashboardAppComponent } from 'projects/dashboard/src/app/app.component';
+import { ChatAppComponent } from 'projects/chat/src/app/app.component';
 
 const routes: Routes = [
-  {path: 'dashboard',
-   loadChildren: '../../projects/dashboard/src/app/app.module#DashboardSharedModule'},
-  {path: 'chat',
-   loadChildren: '../../projects/chat/src/app/app.module#ChatSharedModule'},
-  { path: '**', redirectTo: '/dashboard'}
+  {
+    path: 'dashboard',
+    component: DashboardAppComponent
+  },
+  {
+    path: 'chat',
+    component: ChatAppComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard'
+  }
  ];
 
 @NgModule({
